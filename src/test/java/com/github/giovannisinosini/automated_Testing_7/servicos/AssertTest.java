1a3bec15b87227abbd722f3b8429d5028c60360b
Giovanni Sinosini <carvalho.sino@gmail.com>
2021-02-14 09:22:10 +0000

    Exploring JUnit's Assert method

---
+++ src/test/java/com/github/giovannisinosini/automated_Testing_7/servicos/AssertTest.java
@@ -0,0 +1,50 @@
+package com.github.giovannisinosini.automated_Testing_7.servicos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.giovannisinosini.automated_Testing_7.entidades.Usuario;
+
+public class AssertTest {
+	
+	@Test
+	public void test() {
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		Assert.assertEquals("Erro de comparação ", 1, 1);
+		Assert.assertEquals(0.512345, 0.512, 0.001);  // 0.001 = number of decimal places to be included in the comparison
+		Assert.assertEquals(Math.PI, 3.14, 0.01);
+		
+		// primitive types
+		
+		int i = 11;
+		Integer i2 = 11;
+		
+		Assert.assertEquals(Integer.valueOf(i), i2);
+		Assert.assertEquals(i, i2.intValue());
+		
+		// string test
+		
+		Assert.assertEquals("casa", "casa");
+		Assert.assertNotEquals("casa", "Casa");
+		// Assert.assertEquals("casa", "Casa");
+		Assert.assertTrue("casa".equalsIgnoreCase("Casa"));
+		Assert.assertTrue("casa".startsWith("ca"));
+		
+		// object test
+		
+		Usuario u1 = new Usuario("Usuario 1"); 
+		Usuario u2 = new Usuario("Usuario 1");
+		Usuario u3 = u2;
+		Usuario u4 = null;
+		
+		Assert.assertEquals(u1, u2);  // content testing
+		Assert.assertSame(u2, u3);  // instance testing
+		Assert.assertNotSame(u1, u3);
+		
+		Assert.assertNull(u4);
+		Assert.assertNotNull(u1);
+	}
+
+}
--- src/test/java/com/github/giovannisinosini/automated_Testing_7/servicos/LocacaoService_Teste.java
+++ src/test/java/com/github/giovannisinosini/automated_Testing_7/servicos/LocacaoService_Teste.java
@@ -1,8 +1,13 @@
 package com.github.giovannisinosini.automated_Testing_7.servicos;
 
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.Date;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,9 +28,18 @@ public class LocacaoService_Teste {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
 		// verificacao/validacao
-		Assert.assertTrue(locacao.getValor() == 5.0);
+		
+		//Assert.assertEquals(5.0, locacao.getValor(), 0.01);
+		//Assert.assertThat(locacao.getValor(), CoreMatchers.is(5.0));
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(CoreMatchers.not(6.0)));
+		
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+
+		
 	}
 }
